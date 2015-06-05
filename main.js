49a8deb3a4083a9d3d9ab497be59d1b542079855
Fernando Daciuk <fdaciuk@users.noreply.github.com>
2015-06-04 22:22:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,23 @@
 ;(function( window, document, undefined ) {
-
   'use strict';
 
   function app() {
-
     var $private = {};
     var $public = {};
 
-    /**
-     * Private Properties
-     */
     $private.privateVar = 'private var';
 
-    // -----------------------------------
-
-    /**
-     * Public Methods
-     */
     $public.publicMethod = function() {
       return 'Init';
     };
 
-    // -----------------------------------
-
-    /**
-     * Private Methods
-     */
     $private.privateMethod = function() {
       return 'Private method';
     };
 
-    // -----------------------------------
-
     return $public;
-
   }
 
-  // Global
   window.MyGlobalObject = window.MyGlobalObject || {};
   window.MyGlobalObject.app = app();
-
 })( window, document );
